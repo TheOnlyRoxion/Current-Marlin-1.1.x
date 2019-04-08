eea45546515d45698b6abb217c4d4a13f9d3d713
TheOnlyRoxion <tobiasnixdorf@Tobias-MacBook-Pro-251.local>
2019-04-08 22:51:01 +0200

    jk

--- Marlin/thermistortable_501.h
+++ Marlin/thermistortable_501.h
@@ -20,14 +20,38 @@
  *
  */
 
-// Pt1000 with 4k7 pullup
-const short temptable_1047[][2] PROGMEM = {
-  // only a few values are needed as the curve is very flat
-  PtLine(  0, 1000, 4700)
-  PtLine( 50, 1000, 4700)
-  PtLine(100, 1000, 4700)
-  PtLine(150, 1000, 4700)
-  PtLine(200, 1000, 4700)
-  PtLine(250, 1000, 4700)
-  PtLine(300, 1000, 4700)
+// 100k Zonestar thermistor. Adjusted By Hally
+const short temptable_501[][2] PROGMEM = {
+   {OV(   1), 713},
+   {OV(  14), 300}, // Top rating 300C
+   {OV(  16), 290},
+   {OV(  19), 280},
+   {OV(  23), 270},
+   {OV(  27), 260},
+   {OV(  31), 250},
+   {OV(  37), 240},
+   {OV(  47), 230},
+   {OV(  57), 220},
+   {OV(  68), 210},
+   {OV(  84), 200},
+   {OV( 100), 190},
+   {OV( 128), 180},
+   {OV( 155), 170},
+   {OV( 189), 160},
+   {OV( 230), 150},
+   {OV( 278), 140},
+   {OV( 336), 130},
+   {OV( 402), 120},
+   {OV( 476), 110},
+   {OV( 554), 100},
+   {OV( 635),  90},
+   {OV( 713),  80},
+   {OV( 784),  70},
+   {OV( 846),  60},
+   {OV( 897),  50},
+   {OV( 937),  40},
+   {OV( 966),  30},
+   {OV( 986),  20},
+   {OV(1000),  10},
+   {OV(1010),   0}
 };
